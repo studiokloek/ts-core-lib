b6a5589e4cc997e393ddd3824401dfdb3d14a874
Martijn Swart <martijn@studiokloek.nl>
2019-10-29 21:48:16 +0100

    Tweaks

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+export * from './debug';
+export * from './logger';
+export * from './events';
+
+export * from './ticker';
+export * from './delay';
+export * from './tween';
+
+export * from './device';
+export * from './screen';
+
+export * from './loaders';
+export * from './data';
+export * from './media';
+
+export * from './app';
+export * from './filters';
+
+export * from './html';
+export * from './ui';
+
+export * from './util';
+
+import { initNetworkStatusDetection, getDeviceInfo } from './device';
+
+export async function initCoreLibrary(): Promise<void> {
+  await Promise.all([initNetworkStatusDetection(), getDeviceInfo()]);
+}
