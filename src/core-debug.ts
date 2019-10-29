15b0047d45d44b0d8edaf3ac1ea25a2c169ab49d
Martijn Swart <martijn@studiokloek.nl>
2019-10-29 12:32:10 +0100

    Relative import

--- src/core-debug.ts
+++ src/core-debug.ts
@@ -1,4 +1,4 @@
-import { LogLevels } from '@studiokloek/ts-core-lib';
+import { LogLevels } from './logger/levels';
 import { assign } from 'lodash-es';
 import { settings } from 'pixi.js-legacy';
 
