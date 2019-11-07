2602d05dd9f1d4ad18b8999998994384a5b25117
Martijn Swart <martijn@studiokloek.nl>
2019-11-07 13:43:26 +0100

    Random fixes

--- src/util/random.ts
+++ src/util/random.ts
@@ -1,9 +1,9 @@
-import { Random, MersenneTwister19937 } from 'random-js';
+import { Random as RandomJS, MersenneTwister19937 } from 'random-js';
 
-export type Random = typeof Random;
+export type Random = RandomJS;
 
-export const KloekRandom = new Random(MersenneTwister19937.autoSeed());
+export const KloekRandom = new RandomJS(MersenneTwister19937.autoSeed());
 
 export function createRandomizer(_seed: number): Random {
-  return new Random(MersenneTwister19937.seed(_seed));
+  return new RandomJS(MersenneTwister19937.seed(_seed));
 }
