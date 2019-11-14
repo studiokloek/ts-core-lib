d44c8eb88c9703108202490086387fe8f5e3ffda
Martijn Swart <martijn@studiokloek.nl>
2019-11-14 18:12:45 +0100

    update exports form stage

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -1,7 +1,10 @@
 import { Screen as ScreenClass } from './screen';
 import { ConcreteStage as StageClass } from './stage';
 
-export { RendererOptions, SizeOptions, StageOptions } from './stage';
+import { RendererOptions as RendererOptions2, SizeOptions as SizeOptions2, StageOptions as StageOptions2 } from './stage';
+export type RendererOptions = RendererOptions2;
+export type SizeOptions = SizeOptions2;
+export type StageOptions = StageOptions2;
 
 export const Screen = new ScreenClass();
 export const Stage = new StageClass();
