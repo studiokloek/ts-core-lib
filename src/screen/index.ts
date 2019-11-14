ed13b76f55316f29f804a87c572d49fa5e5e3411
Martijn Swart <martijn@studiokloek.nl>
2019-11-14 14:10:06 +0100

    Export options

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -1,6 +1,8 @@
 import { Screen as ScreenClass } from './screen';
 import { ConcreteStage as StageClass } from './stage';
 
+export { RendererOptions, SizeOptions, StageOptions } from './stage';
+
 export const Screen = new ScreenClass();
 export const Stage = new StageClass();
 
