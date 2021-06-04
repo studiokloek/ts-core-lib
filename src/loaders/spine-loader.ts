4a1ea9bf3b3372dcca3e75691f03457865f90892
Martijn Swart <martijn@studiokloek.nl>
2021-06-04 11:53:55 +0200

    Fix for new pixi-spine

--- src/loaders/spine-loader.ts
+++ src/loaders/spine-loader.ts
@@ -1,5 +1,5 @@
 import { last, split } from 'lodash-es';
-import { SkeletonData } from 'pixi-spine';
+import { ISkeletonData } from 'pixi-spine';
 import { Loader, Texture } from 'pixi.js';
 import { AssetLoaderInterface } from '.';
 import { getLogger } from '../logger';
@@ -9,7 +9,7 @@ const Logger = getLogger('loader > spine');
 
 export interface SpineAsset {
   id: string;
-  skeleton: SkeletonData;
+  skeleton: ISkeletonData;
 }
 
 export interface SpineAssetInfo {
