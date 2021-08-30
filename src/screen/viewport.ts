82562cd0ca21cf9d24d2674bdf7144eb430670f5
Martijn Swart <martijn@studiokloek.nl>
2021-08-30 14:10:51 +0200

    [ts] fix checking errors

--- src/screen/viewport.ts
+++ src/screen/viewport.ts
@@ -8,8 +8,8 @@ export const getViewportSize = memoize(() => {
     viewportSize = Math.max(window.screen.availHeight, window.screen.availWidth);
   } else {
     const topWindow = get(window, 'top', window),
-      width = Math.max(get(topWindow, 'document.documentElement.clientWidth', 0), topWindow.innerWidth || 0),
-      height = Math.max(get(topWindow, 'document.documentElement.clientHeight', 0), topWindow.innerHeight || 0);
+      width = Math.max(get(topWindow, 'document.documentElement.clientWidth', 0), topWindow?.innerWidth || 0),
+      height = Math.max(get(topWindow, 'document.documentElement.clientHeight', 0), topWindow?.innerHeight || 0);
     viewportSize = Math.max(width, height);
   }
   return viewportSize;
