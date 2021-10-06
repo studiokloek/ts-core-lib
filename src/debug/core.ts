078ddf97edf4f08c4ef4aee7b92f648ebd24717f
Martijn Swart <martijn@studiokloek.nl>
2021-10-06 10:35:36 +0200

    [logger] Minimale log level versimpelt

--- src/debug/core.ts
+++ src/debug/core.ts
@@ -40,9 +40,6 @@ export const CoreDebug = {
     return values.isEnabled && values.forceLowResolution;
   },
   getLogLevel(): number {
-    if (values.isEnabled) {
-      return LogLevels.DEBUG;
-    }
     return values.minLogLevel;
   },
   getGlobalTimescale(): number {
