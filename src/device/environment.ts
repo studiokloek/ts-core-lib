231a07c098d26ff4529569fb160058de07a8e136
Martijn Swart <martijn@studiokloek.nl>
2022-08-17 15:39:57 +0200

    [env] Add check to see if we are running on localhost

--- src/device/environment.ts
+++ src/device/environment.ts
@@ -11,3 +11,15 @@ export const isInFrame = memoize(() => {
 
   return inFrame;
 });
+
+export const isLocalhost = memoize(() => {
+  const hostname = window.location.hostname;
+
+  return Boolean(
+    hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      /^127(?:\.(?:25[0-5]|2[0-4]\d|[01]?\d{1,2})){3}$/.test(hostname),
+  );
+});
