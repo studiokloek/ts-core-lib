89d9b324c0c042eccd425fd5de949261a67ed7e8
Martijn Swart <martijn@studiokloek.nl>
2023-10-25 14:21:03 +0200

    [tween] Add modified pixi gsap plugin

--- src/tween/index.ts
+++ src/tween/index.ts
@@ -1,3 +1,13 @@
+// GSAP & TWEEN
+import { gsap } from 'gsap';
+gsap.defaults({ overwrite: 'auto' });
+
+// PIXI en plugins
+import * as PIXI from 'pixi.js';
+import { PixiPlugin } from './pixi-plugin.js';
+PixiPlugin.registerPIXI(PIXI);
+gsap.registerPlugin(PixiPlugin);
+
 export * from './ease';
 export * from './tween';
 export * from './mixin';
